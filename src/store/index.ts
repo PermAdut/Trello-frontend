2b1be7ed8859fcd53a3ccfa0b6bceece683b6e5a
PermAdut <hdvhdvhdvwww@gmail.com>
2025-08-24 11:15:02 +0300

    feat: added task reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,14 @@ import authReducer from './slices/authSlice'
 import logsReducer from './slices/logsSlice'
 import tableReducer from './slices/tableSlice'
 import listReducer from './slices/listSlice'
+import taskReducer from './slices/taskSlice'
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     logs: logsReducer,
     table: tableReducer,
     list: listReducer,
+    task: taskReducer,
   },
 })
 export type RootState = ReturnType<typeof store.getState>
